90462103f62d0fd5c62df74168d50853089741ef
Marco RS <marsucsb@gmail.com>
2018-12-14 21:56:43 -0600

    Update Ui

--- app/src/main/java/com/droidtitan/wordsample/WordsDI.kt
+++ app/src/main/java/com/droidtitan/wordsample/WordsDI.kt
@@ -4,7 +4,6 @@ import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
-
 val wordsModule = module {
   factory { WordRoomDatabase.getDatabase(androidApplication()).wordDao() }
   factory { WordRepository(get()) }
