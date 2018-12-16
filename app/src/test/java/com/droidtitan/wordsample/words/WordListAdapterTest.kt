ea506471e1c0e4b034c4c1712f7a20d2e4b1545c
Marco RS <marsucsb@gmail.com>
2018-12-16 16:36:29 -0600

    Add robolectric.

---
+++ app/src/test/java/com/droidtitan/wordsample/words/WordListAdapterTest.kt
@@ -0,0 +1,23 @@
+package com.droidtitan.wordsample.words
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.droidtitan.wordsample.data.Word
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class WordListAdapterTest {
+
+  private val context = ApplicationProvider.getApplicationContext<Context>()
+
+  @Test fun getItemCount() {
+    val adapter = WordListAdapter(context)
+    assertEquals(0, adapter.itemCount)
+
+    adapter.setWords(arrayListOf(Word("word"), Word("another")))
+    assertEquals(2, adapter.itemCount)
+  }
+}
