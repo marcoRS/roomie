fa14e79891955adb8f4ae93e3b6e73fb0b5e3d24
Marco RS <marsucsb@gmail.com>
2019-02-03 21:14:40 -0600

    Simplify coroutine setup.

--- app/src/main/java/com/droidtitan/roomie/words/WordsViewModel.kt
+++ app/src/main/java/com/droidtitan/roomie/words/WordsViewModel.kt
@@ -4,30 +4,35 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.droidtitan.roomie.data.Word
 import com.droidtitan.roomie.data.WordRepository
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class WordsViewModel(private val repository: WordRepository) : ViewModel() {
+class WordsViewModel(private val repository: WordRepository) : ViewModel(), CoroutineScope {
+  private val job = Job() // Used to explicitly cancel all coroutines in this scope.
 
-  private val job = Job()
-  private val uiScope = CoroutineScope(Dispatchers.Main + job)
+  override val coroutineContext: CoroutineContext
+    get() = Dispatchers.Main + job
 
   val allWords: LiveData<List<Word>>
     get() = repository.allWords
 
   fun insert(word: String) {
-    uiScope.launch {
+    launch(Dispatchers.IO) {
       repository.insert(word)
+    }
+  }
+
+  fun deleteAllWords() {
+    launch(Dispatchers.IO) {
+      repository.deleteAllWords()
     }
   }
 
   override fun onCleared() {
     super.onCleared()
     job.cancel()
-  }
-
-  fun deleteAllWords() {
-    uiScope.launch {
-      withContext(Dispatchers.IO) { repository.deleteAllWords() }
-    }
   }
 }
