e664181bcec0bd048de90ca1740e9cf36d8c2885
Marco RS <marco.rodriguez-suarez@savvyapps.com>
2020-08-17 21:23:31 -0500

    Simplify home activity. Move toolbar into Words Fragment

--- app/src/main/java/com/droidtitan/roomie/words/WordsFragment.kt
+++ app/src/main/java/com/droidtitan/roomie/words/WordsFragment.kt
@@ -1,16 +1,15 @@
 package com.droidtitan.roomie.words
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.droidtitan.roomie.R
-import com.droidtitan.roomie.add.AddWordsFragment
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_words.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
@@ -19,23 +18,23 @@ class WordsFragment : Fragment() {
   private val viewModel: WordsViewModel by sharedViewModel()
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedState: Bundle?): View? {
-    setHasOptionsMenu(true)
     return inflater.inflate(R.layout.fragment_words, container, false)
   }
 
   override fun onViewCreated(view: View, savedState: Bundle?) {
     super.onViewCreated(view, savedState)
-    val adapter = WordListAdapter(view.context)
 
-    adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
-      override fun onChanged() {
-        super.onChanged()
-        emptyView.visibility = if (adapter.itemCount == 0) View.VISIBLE else View.INVISIBLE
-      }
-    })
+    setupToolbar()
 
-    fab.setOnClickListener {
-      findNavController().navigate(R.id.AddNewWord)
+    fab.setOnClickListener { findNavController().navigate(R.id.AddNewWord) }
+
+    val adapter = WordListAdapter(view.context).apply {
+      registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+        override fun onChanged() {
+          super.onChanged()
+          emptyView.isVisible = this@apply.itemCount == 0
+        }
+      })
     }
 
     recyclerview.adapter = adapter
@@ -46,30 +45,14 @@ class WordsFragment : Fragment() {
     }
   }
 
-  override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    super.onCreateOptionsMenu(menu, inflater)
-    inflater.inflate(R.menu.menu_main, menu)
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean =
-    when (item.itemId) {
-      R.id.delete_all -> viewModel.deleteAllWords().let { true }
-      else -> super.onOptionsItemSelected(item)
+  private fun setupToolbar() {
+    toolbar.setTitle(R.string.app_name)
+    toolbar.inflateMenu(R.menu.menu_home)
+    toolbar.setOnMenuItemClickListener { item ->
+      when (item.itemId) {
+        R.id.delete_all -> viewModel.deleteAllWords().let { true }
+        else -> false
+      }
     }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-
-    if (resultCode != RESULT_OK) {
-      Snackbar.make(fab, R.string.empty_not_saved, Snackbar.LENGTH_LONG).show()
-    }
-
-    if (requestCode == NEW_WORD_REQUEST_CODE && resultCode == RESULT_OK) {
-      data?.getStringExtra(AddWordsFragment.EXTRA_REPLY)?.apply { viewModel.insert(this) }
-    }
-  }
-
-  companion object {
-    const val NEW_WORD_REQUEST_CODE = 1
   }
 }
